45c07125871846da30ac66b513d3f123d58e30e3
Maharudra Kekan <41204260+rudra143@users.noreply.github.com>
2020-05-05 19:39:26 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -83,10 +83,7 @@ function draw(){
 
   if (snakeX<0 || snakeY<0 || snakeX>cvs.width/10-1 || snakeY>cvs.height/snakeH-1) {
     alert('Game Over');
-    var snakeX = snake[0].x;
-    var snakeY = snake[0].y;
-
-    location.reload();
+    window.location.reload
   }
 
   //if(snakeX<0){snakeX = cvs.width/snakeW} //
